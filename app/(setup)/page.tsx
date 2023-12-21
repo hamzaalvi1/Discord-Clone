d8c2559df6f09afd0a679747ed3eaed307da2310
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-12-21 12:44:38 +0500

    working on sever modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,12 +1,23 @@
+import { prismadb } from "@/libs/prismadb";
+import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/app/actions";
 import { ToggleMode } from "@/components/ToggleMode";
 const HomePage = async () => {
-  const user = await getCurrentUser(); 
-  console.log(user,"------>user");
- 
+  const user = await getCurrentUser();
+  const server = await prismadb.server.findFirst({
+    where: {
+      members: {
+        some: {
+          userId: user?.id,
+        },
+      },
+    },
+  });
+  if (server?.id) {
+    return redirect(`/servers/${server?.id}`);
+  }
   return (
     <main>
-      Hello HomePage
       <ToggleMode />
     </main>
   );
